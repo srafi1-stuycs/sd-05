d0f7b651bd33f7901510876fee35b26379e54f52
srafi2 <srafi1@stuy.edu>
2017-09-26 10:14:29 -0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,21 @@
+import csv
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+def read_csv():
+    with open('data/occupations.csv', 'rU') as f:
+        reader = csv.reader(f, delimiter=",", quotechar='"')
+        occupations = []
+        for row in reader:
+            occupations.append(row)
+        return occupations
+
+
+@app.route('/occupations')
+def occupations():
+    return render_template('occupations.html',
+            occupations = read_csv() )
+
+if __name__ == '__main__':
+    app.run(debug=True)
