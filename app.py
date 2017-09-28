cb694bc3f98ff0f2492698ff9a4a7187fa5cdf26
aroktim@stuy.edu <aroktim@stuy.edu>
2017-09-28 12:06:51 -0400

    Made util package and made read_csv() take a file name input

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-import csv, occupation
+import csv
+from util import occupation
 from flask import Flask, render_template
 app = Flask(__name__)
 
@@ -12,7 +13,7 @@ def index():
 @app.route('/occupations')
 def occupations():
     return render_template('occupations.html',
-            occupations = occupation.read_csv(),
+            occupations = occupation.read_csv('data/occupations.csv'),
             rand_occupation = occupation.ret_rand() )
 
 if __name__ == '__main__':
